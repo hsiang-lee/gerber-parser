170e84d9e7ec78719da5d80227392515b0a8c6ad
leehsiang <leehsiang@sina.com>
2022-11-26 20:08:11 +0800

    refactor: replace old list with std::list

--- src/macos/cmake/gerber-parserTargets-release.cmake
+++ src/macos/cmake/gerber-parserTargets-release.cmake
@@ -1,16 +1,16 @@
 #----------------------------------------------------------------
-# Generated CMake target import file.
+# Generated CMake target import file for configuration "Release".
 #----------------------------------------------------------------
 
 # Commands may need to know the format version.
 set(CMAKE_IMPORT_FILE_VERSION 1)
 
-# Import target "gerber_parser" for configuration ""
-set_property(TARGET gerber_parser APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+# Import target "gerber_parser" for configuration "Release"
+set_property(TARGET gerber_parser APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(gerber_parser PROPERTIES
-  IMPORTED_LINK_DEPENDENT_LIBRARIES_NOCONFIG "Qt5::Core;Qt5::Widgets;Qt5::Gui"
-  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/macos/lib/libgerber_parser.dylib"
-  IMPORTED_SONAME_NOCONFIG "@rpath/libgerber_parser.dylib"
+  IMPORTED_LINK_DEPENDENT_LIBRARIES_RELEASE "Qt5::Core;Qt5::Widgets;Qt5::Gui"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/macos/lib/libgerber_parser.dylib"
+  IMPORTED_SONAME_RELEASE "@rpath/libgerber_parser.dylib"
   )
 
 list(APPEND _cmake_import_check_targets gerber_parser )
