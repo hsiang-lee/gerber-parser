00d630c63d67e7fa9010977a8f3f404aeb64262c
leehsiang <leehsiang@sina.com>
2023-03-01 07:32:48 +0800

    feat: update tool gerber2image

--- example/gerber2image/main.cpp
+++ example/gerber2image/main.cpp
@@ -2,6 +2,8 @@
 #include <QFileDialog>
 #include <QPixmap>
 
+#include <iostream>
+
 #include "gerber_parser/bound_box.h"
 #include "gerber/gerber.h"
 #include "gerber_parser/gerber_parser.h"
@@ -19,46 +21,29 @@ int main(int argc, char *argv[])
 
 	QApplication app(argc, argv);
 
-	auto parser = std::make_shared<GerberParser>(FLAGS_gerber_file);
-	auto gerber = parser->GetGerber();
-	const auto width_pixel = gerber->GetBBox().Width() * 1000 / FLAGS_um_pixel;
-	const auto height_pixel = gerber->GetBBox().Height() * 1000 / FLAGS_um_pixel;
-
-	int width_img = 0;
-	int height_img = 0;
-	if (width_pixel > height_pixel)
-	{
-		height_img = 20000 * height_pixel / width_pixel;
-		width_img = 20000;
-	}
-	else
-	{
-		width_img = 20000 * width_pixel / height_pixel;
-		height_img = 20000;
+	if (FLAGS_gerber_file.empty()) {
+          std::cout << "Usage: gerber2image --gerber_file=\"path/to/gerber/file\" --um_pixel=5" << std::endl;
+          return 0;
 	}
 
-	auto image = std::make_unique<QPixmap>(width_img * 1.05, height_img * 1.05);
-	auto engine = std::make_unique<QPainterEngine>(image.get(), gerber->GetBBox(), BoundBox(0.0, 0.0, 0.0, 0.0));
+	try {
+          auto parser = std::make_shared<GerberParser>(FLAGS_gerber_file);
+          auto gerber = parser->GetGerber();
+          const auto width_pixel = 2560;
+          const auto height_pixel = int(double(gerber->GetBBox().Height()) /
+                                        gerber->GetBBox().Width() * 2560);
 
-	int height_scale = (height_pixel - 1) / height_img + 1;
-	int width_scale = (width_pixel - 1) / width_img + 1;
-
-	engine->Scale(std::max(width_scale, height_scale) - 1);
-
-	for (int i = 0; i < height_scale; ++i)
-	{
-		for (int j = 0; j < width_scale; ++j)
-		{
-			engine->RenderGerber(gerber);
-
-			auto image_file = QString(FLAGS_gerber_file.c_str()) + '_' + QString("%1").arg(i) + '_' + QString("%1").arg(j) + ".jpg";
-			image->save(image_file);
-
-			engine->Move(-width_img, 0);
-		}
-
-		engine->Move(width_img * width_scale, -height_img);
-	}
+          auto image = std::make_unique<QPixmap>(width_pixel * 1.05,
+                                                 height_pixel * 1.05);
+          auto engine = std::make_unique<QPainterEngine>(
+              image.get(), gerber->GetBBox(), BoundBox(0.05, 0.05, 0.05, 0.05));
+          engine->RenderGerber(gerber);
+          auto image_file = QString(FLAGS_gerber_file.c_str()) + ".png";
+          image->save(image_file);
+    }
+    catch (const std::exception& e) {
+          std::cout << e.what() << std::endl;
+    }
 
 	return 0;
 }
