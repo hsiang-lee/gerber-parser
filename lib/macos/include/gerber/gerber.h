d31a21056d7b3852bbfd0fabcec24933db4048ae
hsiang-lee <leehsiang@sina.com>
2023-09-23 20:24:25 +0800

    Optimize cpu usage

--- lib/macos/include/gerber/gerber.h
+++ lib/macos/include/gerber/gerber.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <memory>
 #include <string>
 #include <vector>
 
@@ -9,38 +8,47 @@
 class Aperture;
 class GerberLayer;
 
-class UnitType {
- public:
-  enum Type { guInches, guMillimeters };
+class UnitType
+{
+public:
+    enum Type
+    {
+        guInches,
+        guMillimeters
+    };
 
-  static double Get_mm(double number, Type unit);
+    static double Get_mm(double number, Type unit);
 };
 
-class GerberApi Gerber {
- public:
-  Gerber() {
-    unit_ = UnitType::guInches;
-    apertures_.resize(3000);  // NOLINT
-  }
+class GerberApi Gerber
+{
+public:
+    Gerber()
+    {
+        unit_ = UnitType::guInches;
+        apertures_.resize(3000); // NOLINT
+    }
 
-  bool negative_{false};
-  bool IsNegative() const;
+    ~Gerber();
 
-  UnitType::Type unit_{UnitType::guMillimeters};
-  UnitType::Type Unit() const;
+    bool negative_{false};
+    bool IsNegative() const;
 
-  BoundBox GetBBox() const;
-  std::string Name() const;
-  void SetName(const std::string& name);
+    UnitType::Type unit_{UnitType::guMillimeters};
+    UnitType::Type Unit() const;
 
-  std::shared_ptr<Aperture> GetAperture(int code) const;
-  void SetAperture(int code, std::shared_ptr<Aperture> aperture);
+    BoundBox GetBBox() const;
+    std::string Name() const;
+    void SetName(const std::string &name);
 
-  const std::vector<std::shared_ptr<GerberLayer>>& GetLayers() const;
-  void AddLayer(std::shared_ptr<GerberLayer> layer);
+    Aperture *GetAperture(int code) const;
+    void SetAperture(int code, Aperture *aperture);
+
+    const std::vector<GerberLayer *> &GetLayers() const;
+    void AddLayer(GerberLayer *layer);
 
 private:
-  std::vector<std::shared_ptr<GerberLayer>> layers_;
-  std::vector<std::shared_ptr<Aperture>> apertures_;
-  std::string name_;
+    std::vector<GerberLayer *> layers_;
+    std::vector<Aperture *> apertures_;
+    std::string name_;
 };
